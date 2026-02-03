232fab69c23162080007ebb375a63e685166e5e0
Najla-Citadel <nurnafisah.sharudin@citadelgroup.com.my>
2026-02-03 11:30:24 +0800

    added workflow for github
    - to makesure website can read next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // REQUIRED for GitHub Pages
+  },
+  basePath: "/Fraudshield_Website",
+  assetPrefix: "/Fraudshield_Website/",
 };
 
 export default nextConfig;
