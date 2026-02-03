a85b84aa625b9b79bc24fb005954d73413b9692c
Najla-Citadel <nurnafisah.sharudin@citadelgroup.com.my>
2026-02-03 11:49:34 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // REQUIRED for GitHub Pages
+  },
   basePath: "/Fraudshield_Website",
   assetPrefix: "/Fraudshield_Website/",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
